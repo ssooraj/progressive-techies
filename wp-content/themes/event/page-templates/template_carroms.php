87e290a5a626efc4c4556dda0c3bc001d14fe536
Sooraj S <ssooraj7@gmail.com>
2019-02-20 15:39:11 +0530

    update on feb 20 2019

---
+++ wp-content/themes/event/page-templates/template_carroms.php
@@ -0,0 +1,117 @@
+<?php
+	/**
+	 *
+	 * Template Name: Carroms Template
+	 */
+	?>
+<?php get_header(); ?>
+<div class="home carroms">
+	<div class="top-banner carroms">
+		<h1>Techies Carroms Tournament</h1>
+	</div>
+	<div class="content-wrap">
+		<div class="about">
+			<h2>about the event</h2>
+			<div class="details">
+				<p>Progressive Techies are conducting a Techies Carroms Tournament on 16th march, 2019 at United Sports Centre, Kakkanad. Employees of IT companies can compete in General(Mixed or Men's) &Women's Doubles </p>
+				</br>
+				<p>Don't wait up, register today itself with your colleagues and start practicing! It's your chance to show who's the best!</p>
+			</div>
+			<!-- <div class="banner">
+				<img src="/wp-content/themes/event/poster1.jpg" alt="">
+			</div> -->
+			<div class="poster">
+		<img src="/wp-content/themes/event/car-banner.jpg" alt="">
+	</div>
+		</div>
+		<div class="left-wrap">
+			<div class="content">
+				<h2>Rules and Regular Guidelines</h2>
+				<hr>
+				<ol>
+					<h3>General Rules</h3>
+					<hr class="second">
+					<li>The tournament is open to all individuals working in IT companies.</li>
+					<li> The tournament comprises of below events: </li>
+					<ul>
+                        <li>General ( Men's &amp; Mixed)</li>
+                        <li>Women's Doubles</li>
+					</ul>
+					<li>Players from Non-IT companies won’t be entertained.</li>
+					<li>Players can participate in more than one category/event.</li>
+					<li>Both the players in a team should belong to same organisation.</li>
+					<li>Participants have to produce their company/ organisation ID card.</li>
+					<li>Participants need to send an e-mail from official mail id to
+						progressive.techies@gmail.com –OR- confirmation on official letter head about the
+						participants while reporting for their matches.
+					</li>
+					<li>Participants from companies outside Kakkanad should bring HR letter confirming
+						their employment.
+					</li>
+					<li>Participants have to reveal their HR contact details at any point of time on demand.</li>
+					<li>Any dispute regarding any teams should be disclosed to organisers first.</li>
+					<li>Team will be disqualified at any point of time on finding any dispute true and legal
+						actions will be taken.
+					</li>
+					<li>For all matches, players / teams to report at least 30 minutes before
+						scheduled time.
+					</li>
+					<li>Players to be in proper playing attire.</li>
+					<li>For any matter related to conduct of matches, contact Referee.</li>
+					<li>The registration fee & the details regarding participants have to reach the organisers
+						on or before 12th March 2019
+					</li>
+					<li>Registration would be considered as complete only if the registration form is filled
+						with your details, mode of payment is selected and the amount is paid on or before
+						12th March 2019 and mail confirmation is send from official mail.
+					</li>
+					<h3>Payment</h3>
+					<hr class="second">
+					<li>The Registration fee per team for doubles category is <b>Rs.200/-</b>
+                    </li>
+                    <li>The registration amount may be paid by cash / ‘Google Pay’ payment</li>
+                    <ul>
+                   
+                    </ul>
+						<h3>Disclaimer</h3>
+						<hr class="second">
+					<li>Decision of the Referees & the Organisers would be final & binding.</li>
+					<li>Organisers will have the authority to cancel any event in case the number of
+						registrations is less than expected for that event. In such cases, the registration
+						charges will be refunded.
+					</li>
+				</ol>
+			</div>
+		</div>
+		<div class="right-wrap">
+			<div class="content">
+			<div class="panel-body">
+					<div class="media-body">
+						<h4 class="fixture-head">Registration Links</h4>
+						<!-- <p class="fixture-text"><i ><a class="fixture" href="/wp-content/themes/event/fixture.pdf" download>click here</i></a> to download the match fixture</p> -->
+						<!-- <a class="btn btn-info btn-lg" target="_blank" href="https://docs.google.com/forms/d/e/1FAIpQLSf7QqExn1CCKm7KHnLeQlQ94Ojp98pUtkkotfLsShEbaNNQQw/viewform">Register Here</a> -->
+					</div>
+				</div>
+				<div class="panel-body">
+					<div class="media-body">
+						<h4>General (Opposite Gender Allowed)</h4>
+						<p>*Both the players in a team should belong to same organisation</p>
+						<!-- <p class="closed">**Registration Closed**</p> -->
+						<a class="btn btn-info btn-lg" target="_blank" href="https://docs.google.com/forms/d/e/1FAIpQLScuuJPLTky7wjRCFZuvtflfBa8f6kvxmZf7a29MYywD_B7a_w/viewform">Register Here</a>
+					</div>
+					
+				</div>
+				<div class="panel-body">
+					<div class="media-body">
+						<h4>Women's Doubles</h4>
+						<p>*Both the players in a team should belong to same organisation</p>
+						<a class="btn btn-info btn-lg" target="_blank" href="https://docs.google.com/forms/d/e/1FAIpQLSeB5zEH2s6IgaElI0YZ_2LqrXBB877oDYrmglt4bqjuADO6Cg/viewform">Register Here</a>
+						<!-- <p class="closed">**Registration Closed**</p> -->
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	</div>
+</div>
+<!-- <?php //get_footer(); ?> -->
